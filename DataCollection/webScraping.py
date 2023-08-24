30bd6965e34b763d1191789532c29c4b611e9ca3
imsid22 <siddhantd94@gmail.com>
2023-08-24 00:22:53 -0600

    feat: completed data scraping using beautifulsoup

--- DataCollection/webScraping.py
+++ DataCollection/webScraping.py
@@ -7,6 +7,28 @@ URLs = ['https://venturebeat.com/tag/data-science/']
 
 articles = []
 
+def clean_paragraphs(paragraphs):
+    cleaned_paragraphs = [para.replace('\xa0', ' ').strip() for para in paragraphs if para.strip() != '']
+    return cleaned_paragraphs
+
+def fetchContent(link):
+    """This function fetches the content from the given link
+
+    Args:
+        link (str): link of the article
+    """
+    response = requests.get(link)
+    response_soup = BeautifulSoup(response.content, 'html.parser')
+    link_result = response_soup.find(id='content')
+    content = link_result.find_all(class_='article-content')
+    paragraphs = []
+    for content_div in content:
+        for p in content_div.find_all('p'):
+            paragraphs.append(p.get_text().replace('\xa0', ' ').replace('“', '').replace('”', '').strip())
+
+    return ' '.join(paragraphs)
+
+counter = 0
 for url in URLs:
     # Fetch webpage content
     response = requests.get(url)
@@ -15,13 +37,15 @@ for url in URLs:
     # Locate articles (assuming they are within the articles tag)
     results = soup.find(id='river')
 
-    for article in results.find_all('article', class_='ArticleListing'):
-        
+    for article in results.find_all('a', class_='ArticleListing__title-link'):
+        if counter > 1:
+            break
         title = article.text
+        content = fetchContent(article.get('href'))
 
         articles.append({
             'title': title,
-            'content': ''
+            'content': content
         })
-
+        counter += 1
 print(articles)
